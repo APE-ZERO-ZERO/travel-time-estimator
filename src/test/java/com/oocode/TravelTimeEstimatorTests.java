90752be8bbdbf2da0afda0630e2dc3aa281b49a8
APE-ZERO-ZERO <hidden.by@default.com>
2023-06-11 20:40:24 +0200

    Q1 - Added test case for coverage of legacy code.

---
+++ src/test/java/com/oocode/TravelTimeEstimatorTests.java
@@ -0,0 +1,30 @@
+package com.oocode;
+
+import org.junit.*;
+import com.oocode.assignment2023.TravelTimeEstimator;
+
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.core.Is.*;
+
+public class TravelTimeEstimatorTests {
+
+    @Test(timeout = 2000)
+    public void CallingWorksForGivenCoordinates() {
+        String[] locations = {"51.534327", "-0.012768", "51.504674", "-0.086005"};
+        int travelTime;
+        boolean check = false;
+        try {
+            travelTime = TravelTimeEstimator.travelTimeInMinutes(locations);
+            System.out.println(travelTime);
+            if (travelTime >= 25 && travelTime <= 45) {
+                check = true;
+            }
+            assertThat(check, is(true));
+        } catch (Exception e) {
+            //assertThat(false, is(true));
+        }
+    }
+
+
+
+}
